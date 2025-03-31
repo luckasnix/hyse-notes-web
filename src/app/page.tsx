94fd311246abdbfa3a83e63c2fefd51b6420cae0
Kasnix <luc.kasnix@gmail.com>
2025-03-31 11:26:23 -0300

    feat: add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { yellow } from "@mui/material/colors";
 
 export default function Home() {
   return (
-    <Grid container direction="column" alignItems="center">
-      <Typography variant="h1">Hyse Notes</Typography>
+    <Grid container direction="row" justifyContent="center" alignItems="center" spacing={4}>
+      <Image
+        src="/logo.svg"
+        width={128}
+        height={128}
+        alt="Hyse Notes logo"
+      />
+      <Typography variant="h1" sx={{ color: yellow[800] }}>Hyse Notes</Typography>
     </Grid>
   );
 }
