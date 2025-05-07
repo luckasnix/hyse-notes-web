e70660222c81d695b47451d05d499e4175ebf456
Kasnix <luc.kasnix@gmail.com>
2025-05-07 13:52:26 -0300

    feat: add Toast component

---
+++ src/components/toast.tsx
@@ -0,0 +1,33 @@
+import type { ComponentProps } from "react";
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+import type { SxProps, Theme } from "@mui/material/styles";
+
+export const alertStyle: SxProps<Theme> = {
+  width: "100%",
+};
+
+export type ToastOptions = {
+  severity: ComponentProps<typeof Alert>["severity"];
+  message: string;
+};
+
+export type ToastProps = Readonly<
+  {
+    open: boolean;
+    onClose: () => void;
+  } & ToastOptions
+>;
+
+export const Toast = ({ open, onClose, severity, message }: ToastProps) => (
+  <Snackbar open={open} autoHideDuration={4000} onClose={onClose}>
+    <Alert
+      variant="filled"
+      sx={alertStyle}
+      severity={severity}
+      onClose={onClose}
+    >
+      {message}
+    </Alert>
+  </Snackbar>
+);
