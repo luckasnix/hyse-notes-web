a7fb041701870c13340434af51795fd3cd333d22
Kasnix <luc.kasnix@gmail.com>
2025-05-27 16:56:03 -0300

    feat(NoteList): display more information in notes

--- src/components/note-list.tsx
+++ src/components/note-list.tsx
@@ -1,9 +1,12 @@
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import type { SxProps, Theme } from "@mui/material/styles";
 
 import type { Note } from "~/database/local";
+import { convertTimestampToDate } from "~/utils/general";
 
 export const containerStyle: SxProps<Theme> = {
   paddingX: 6,
@@ -12,7 +15,7 @@ export const containerStyle: SxProps<Theme> = {
   flex: 1,
 };
 
-export const textBoxStyle: SxProps<Theme> = {
+export const noteContainerStyle: SxProps<Theme> = {
   padding: 2,
   bgcolor: "background.paper",
   borderRadius: 1,
@@ -25,12 +28,29 @@ export type NoteListProps = Readonly<{
 
 export const NoteList = ({ notes }: NoteListProps) => (
   <Stack direction="column-reverse" spacing={2} sx={containerStyle}>
-    {notes.map(({ id, content }) => (
-      <Box key={id} sx={textBoxStyle}>
-        <Typography variant="body1" whiteSpace="pre-wrap">
-          {content}
-        </Typography>
-      </Box>
+    {notes.map(({ id, content, createdAt }) => (
+      <Stack key={id} sx={noteContainerStyle}>
+        <Stack direction="row">
+          <Box sx={{ flex: 1 }}>
+            <Typography variant="body1" whiteSpace="pre-wrap">
+              {content}
+            </Typography>
+          </Box>
+          <Box>
+            {/* TODO: Add ActionMenu */}
+            <IconButton size="small" onClick={() => console.log(`Note ${id}`)}>
+              <MoreVertIcon />
+            </IconButton>
+          </Box>
+        </Stack>
+        <Stack direction="row">
+          <Box sx={{ flex: 1 }}>
+            <Typography variant="caption" color="text.secondary">
+              {convertTimestampToDate(createdAt)}
+            </Typography>
+          </Box>
+        </Stack>
+      </Stack>
     ))}
   </Stack>
 );
--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,3 +1,13 @@
 export const getTimestampInSeconds = (): number => {
   return Math.floor(Date.now() / 1000);
 };
+
+export const convertTimestampToDate = (timestampInSeconds: number): string => {
+  const date = new Date(timestampInSeconds * 1000);
+
+  return new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+    timeStyle: "long",
+    timeZone: "America/New_York",
+  }).format(date);
+};
