782f937fe9f74e3fe9abb5cdd0a3385ef77560bc
Kasnix <luc.kasnix@gmail.com>
2025-05-28 12:30:39 -0300

    feat: add note removal

--- src/components/note-list.tsx
+++ src/components/note-list.tsx
@@ -1,11 +1,16 @@
+"use client";
+import DeleteIcon from "@mui/icons-material/Delete";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import type { SxProps, Theme } from "@mui/material/styles";
+import { useState, type MouseEvent } from "react";
 
-import type { Note } from "~/database/local";
+import { ActionMenu } from "~/components/action-menu";
+import { useUi } from "~/contexts/ui-context";
+import { localDb, type Note } from "~/database/local";
 import { convertTimestampToDate } from "~/utils/general";
 
 export const containerStyle: SxProps<Theme> = {
@@ -26,31 +31,78 @@ export type NoteListProps = Readonly<{
   notes: Note[];
 }>;
 
-export const NoteList = ({ notes }: NoteListProps) => (
-  <Stack direction="column-reverse" spacing={2} sx={containerStyle}>
-    {notes.map(({ id, content, createdAt }) => (
-      <Stack key={id} sx={noteContainerStyle}>
-        <Stack direction="row">
-          <Box sx={{ flex: 1 }}>
-            <Typography variant="body1" whiteSpace="pre-wrap">
-              {content}
-            </Typography>
-          </Box>
-          <Box>
-            {/* TODO: Add ActionMenu */}
-            <IconButton size="small" onClick={() => console.log(`Note ${id}`)}>
-              <MoreVertIcon />
-            </IconButton>
-          </Box>
+export const NoteList = ({ notes }: NoteListProps) => {
+  const { showToast } = useUi();
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [currentNoteId, setCurrentNoteId] = useState<string | null>(null);
+  const open = Boolean(anchorEl);
+
+  const openMenu = (event: MouseEvent<HTMLButtonElement>, noteId: string) => {
+    setAnchorEl(event.currentTarget);
+    setCurrentNoteId(noteId);
+  };
+
+  const closeMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const deleteNote = async () => {
+    if (currentNoteId) {
+      try {
+        await localDb.notes.delete(currentNoteId);
+        closeMenu();
+      } catch {
+        showToast({
+          severity: "error",
+          message: "Failed to delete note. Please try again.",
+        });
+      }
+    }
+  };
+
+  return (
+    <Stack direction="column-reverse" spacing={2} sx={containerStyle}>
+      {notes.map(({ id, content, createdAt }) => (
+        <Stack key={id} spacing={1} sx={noteContainerStyle}>
+          <Stack direction="row" spacing={1}>
+            <Box sx={{ flex: 1 }}>
+              <Typography variant="body1" whiteSpace="pre-wrap">
+                {content}
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton
+                size="small"
+                onClick={(event) => {
+                  openMenu(event, id);
+                }}
+              >
+                <MoreVertIcon />
+              </IconButton>
+            </Box>
+          </Stack>
+          <Stack direction="row" spacing={1}>
+            <Box sx={{ flex: 1 }}>
+              <Typography variant="caption" color="text.secondary">
+                {convertTimestampToDate(createdAt)}
+              </Typography>
+            </Box>
+          </Stack>
         </Stack>
-        <Stack direction="row">
-          <Box sx={{ flex: 1 }}>
-            <Typography variant="caption" color="text.secondary">
-              {convertTimestampToDate(createdAt)}
-            </Typography>
-          </Box>
-        </Stack>
-      </Stack>
-    ))}
-  </Stack>
-);
+      ))}
+      <ActionMenu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={closeMenu}
+        items={[
+          {
+            type: "error",
+            label: "Delete note",
+            icon: <DeleteIcon />,
+            onClick: deleteNote,
+          },
+        ]}
+      />
+    </Stack>
+  );
+};
