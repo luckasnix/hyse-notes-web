22f0df27a0ee71d400364c9d1bf1365522d85319
Kasnix <luc.kasnix@gmail.com>
2025-06-20 10:00:02 -0300

    feat: add note schema

---
+++ src/schemas/notes.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const noteSchema = z.object({
+  content: z.string({
+    required_error: "Content cannot be empty",
+  }),
+});
+
+export type NoteValues = z.infer<typeof noteSchema>;
