f210eee5229fcb6587ca0192f67102e44dbdf963
Kasnix <luc.kasnix@gmail.com>
2025-06-21 09:39:43 -0300

    feat: add NoteUpdateModal component

---
+++ src/modals/note-update-modal.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Modal from "@mui/material/Modal";
+
+import { NoteForm } from "~/components/note-form";
+import { useUi } from "~/contexts/ui-context";
+import type { Note } from "~/database/local";
+import type { NoteValues } from "~/schemas/notes";
+import { updateNote } from "~/services/notes";
+
+export type NoteUpdateModalProps = Readonly<{
+  note: Note;
+  open: boolean;
+  onClose: () => void;
+}>;
+
+export const NoteUpdateModal = ({
+  note,
+  open,
+  onClose,
+}: NoteUpdateModalProps) => {
+  const { showToast } = useUi();
+
+  const handleSubmit = (values: NoteValues) => {
+    const newNote: Note = {
+      ...note,
+      content: values.content,
+    };
+    updateNote(newNote, undefined, () => {
+      showToast({
+        severity: "error",
+        message: "Failed to update note. Please try again.",
+      });
+    });
+  };
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <NoteForm
+        title="Update note"
+        labels={{ submit: "Update", cancel: "Cancel" }}
+        initialValues={{
+          content: note.content,
+        }}
+        onCancel={onClose}
+        onSubmit={handleSubmit}
+      />
+    </Modal>
+  );
+};
