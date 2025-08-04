6f4b34b5ecd59a8b4c7c6bb1c589b20961d5c774
Kasnix <luc.kasnix@gmail.com>
2025-08-04 14:55:13 -0300

    chore: upgrade Zod to version 4

--- src/schemas/notes.ts
+++ src/schemas/notes.ts
@@ -1,9 +1,7 @@
 import { z } from "zod";
 
 export const noteSchema = z.object({
-  content: z.string({
-    required_error: "Content cannot be empty",
-  }),
+  content: z.string().min(1, "Note must contain at least 1 character"),
 });
 
 export type NoteValues = z.infer<typeof noteSchema>;
