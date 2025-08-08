9c8b42214d2c20747cda5cec5833b145ac5eaf39
Kasnix <luc.kasnix@gmail.com>
2025-08-08 17:39:18 -0300

    feat: add ConfirmationModal component

---
+++ src/modals/confirmation-modal.tsx
@@ -0,0 +1,54 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Stack from "@mui/material/Stack";
+import type { SxProps, Theme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
+const contentStyle: SxProps<Theme> = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 360,
+  bgcolor: "background.paper",
+  borderRadius: 1,
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: 1,
+};
+
+export type ConfirmationModalProps = Readonly<{
+  open: boolean;
+  content: string;
+  labels: {
+    cancel: string;
+    confirm: string;
+  };
+  onCancel: () => void;
+  onConfirm: () => void;
+}>;
+
+export const ConfirmationModal = ({
+  open,
+  content,
+  labels,
+  onCancel,
+  onConfirm,
+}: ConfirmationModalProps) => (
+  <Modal open={open} onClose={onCancel}>
+    <Box sx={contentStyle}>
+      <Typography variant="body1">{content}</Typography>
+      <Stack direction="row" spacing={1} sx={{ justifyContent: "flex-end" }}>
+        <Button variant="outlined" size="small" onClick={onCancel}>
+          {labels.cancel}
+        </Button>
+        <Button variant="contained" size="small" onClick={onConfirm}>
+          {labels.confirm}
+        </Button>
+      </Stack>
+    </Box>
+  </Modal>
+);
