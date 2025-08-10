f0f58d7f332cef89b530017150851aa6decc532d
Kasnix <luc.kasnix@gmail.com>
2025-08-10 15:52:30 -0300

    feat: add logos

--- src/sections/main-section.tsx
+++ src/sections/main-section.tsx
@@ -1,23 +1,14 @@
 import Grid from "@mui/material/Grid";
 import type { SxProps, Theme } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
 import Image from "next/image";
 
 const containerStyle: SxProps<Theme> = {
-  direction: "row",
   justifyContent: "center",
   alignItems: "center",
 };
 
-const titleTextStyle: SxProps<Theme> = {
-  color: "primary.main",
-};
-
 export const MainSection = () => (
   <Grid component="section" container size="grow" sx={containerStyle}>
-    <Image src="/logo.svg" width={128} height={128} alt="Hyse Notes logo" />
-    <Typography variant="h1" sx={titleTextStyle}>
-      Hyse Notes
-    </Typography>
+    <Image src="/logo.svg" width={512} height={128} alt="Hyse Notes logo" />
   </Grid>
 );
