aeb1354f6c997708d567d6209f0e3ce3ec28bbd2
Kasnix <luc.kasnix@gmail.com>
2025-10-05 20:57:16 -0300

    refactor(NotFound): enhance page with Material UI components

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,10 +1,42 @@
-import { Link } from "@tanstack/react-router";
+import HomeIcon from "@mui/icons-material/Home";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import type { SxProps, Theme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "@tanstack/react-router";
 
-// TODO: Improve later
-export const NotFound = () => (
-  <main>
-    <h1>Página não encontrada</h1>
-    <p>Desculpe, a página que você está procurando não existe.</p>
-    <Link to="/">Voltar para a página inicial</Link>
-  </main>
-);
+const containerStyle: SxProps<Theme> = {
+  height: "100dvh",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const textContainer: SxProps<Theme> = {
+  textAlign: "center",
+};
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleBackButtonClick = () => {
+    navigate({ to: "/" });
+  };
+
+  return (
+    <Stack direction="column" spacing={2} sx={containerStyle}>
+      <Stack direction="column" spacing={1} sx={textContainer}>
+        <Typography variant="h3">Page not found</Typography>
+        <Typography variant="body1">
+          Sorry, the page you are looking for does not exist.
+        </Typography>
+      </Stack>
+      <Button
+        variant="contained"
+        startIcon={<HomeIcon />}
+        onClick={handleBackButtonClick}
+      >
+        Back to home
+      </Button>
+    </Stack>
+  );
+};
