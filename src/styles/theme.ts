4d7b53673c407212d5a074a8df89596c4b2be815
Kasnix <luc.kasnix@gmail.com>
2025-10-18 15:05:16 -0300

    feat(theme): add typography styles for headings

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,5 +12,29 @@ export const theme = createTheme({
   },
   typography: {
     fontFamily: "'Noto Sans Variable', sans-serif",
+    h1: {
+      fontSize: "3.5rem",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "3rem",
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: "2.5rem",
+      fontWeight: 500,
+    },
+    h4: {
+      fontSize: "2rem",
+      fontWeight: 500,
+    },
+    h5: {
+      fontSize: "1.75rem",
+      fontWeight: 500,
+    },
+    h6: {
+      fontSize: "1.5rem",
+      fontWeight: 500,
+    },
   },
 });
