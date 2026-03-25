c0c8999195c187971f439341c5759ba0cb2d1892
Kasnix <luc.kasnix@gmail.com>
2026-03-25 19:00:29 -0300

    chore: upgrade dependencies and migrate Vite from 7 to 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const viteConfig = defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tanstackStart(),
-    viteReact(),
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [tanstackStart(), viteReact()],
 });
 
 export default viteConfig;
